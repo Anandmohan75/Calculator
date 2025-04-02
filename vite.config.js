5be4e914c5a76558d04c96050d58b5ff1440c7c8
Anand Mohan Chaudhary <anandmohanchaudhari123@gmail.com>
2025-04-02 22:09:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Ensure correct base path
+  build: {
+    outDir: "dist", // Make sure Vercel finds the correct build folder
+  }
+});
